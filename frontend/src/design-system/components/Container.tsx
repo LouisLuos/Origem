02f3e95a6d9af1b0214180b24f0b162b73e99459
gitviini <gvinicius105@gmail.com>
2026-09-25 13:22:02 -0300

    remove frontend

--- frontend/src/design-system/components/Container.tsx
+++
@@ -1,6 +0,0 @@
-import type { HTMLAttributes } from 'react'
-import { cn } from '../cn'
-
-export function Container({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn('mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8', className)} {...props} />
-}
